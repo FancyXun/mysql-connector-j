4da9e6a85d76c838c282366e2fc5420cadabcc5c
fancyxun <837633751@qq.com>
2022-06-30 10:28:23 +0800

    解析url

--- src/main/core-api/java/com/mysql/cj/sse/DriverManager.java
+++ src/main/core-api/java/com/mysql/cj/sse/DriverManager.java
@@ -5,6 +5,13 @@ public class DriverManager {
     public static Connection getConnection(String url, String username, String password){
 
         Connection connection = null;
+        String ip_port = "";
+        if (url.startsWith("jdbc:mysql")){
+            ip_port = url.split("/")[2];
+        }
+        System.out.println(ip_port);
+
+
         return connection;
     }
 
